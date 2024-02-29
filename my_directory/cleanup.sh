30e47a646b32b9f7aaf79c4daf6ba7de31cab913
timeking23 <danielgetahun46@gmail.com>
2024-02-29 15:13:55 -0600

    Update cleanup.sh

--- my_directory/cleanup.sh
+++ my_directory/cleanup.sh
@@ -8,3 +8,27 @@ do
 		echo "$x  directory not found, Creating $x directory"
 	fi
 done
+
+for file in *; do
+	if test -f "$file" ;then
+        files="${file##*.}"
+        case "$files" in
+            jpg|jpeg|png|gif)
+                echo "images bytes: "
+                stat -c %s $file
+                mv "$file" "images/" ;;
+            txt|docx|doc|pages|key|pptx|ppt|odt|md)
+                mv "$file" "documents/" ;;
+            pdf)
+                mv "$file" "pdfs/" ;;
+            sh|exe)
+                if [ -x "$file" ]; then
+                    mv "$file" "executables/"
+                fi ;;
+            csv|xlsx|json)
+                mv "$file" "data/" ;;
+            *)
+                mv "$file" "unknown/" ;;
+        esac
+    fi    
+done    
