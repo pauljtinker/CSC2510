bb9308dfb2208e2c98239de4ff6f137af0f36179
timeking23 <danielgetahun46@gmail.com>
2024-03-11 12:06:09 -0500

    fixed syntax

--- class/classlist.sh
+++ class/classlist.sh
@@ -1,23 +1,24 @@
 #!/bin/bashgi
 #) verify correct number of arguments was received
-if (($# -lt 2 )):
-    echo invalid augment
-    break;
-elif (($# -gt 3)):
-    echo invalid augment
-    break;
+if [ $# -lt 2 ]; then
+    echo Invalid number of arguments.
+    exit 1
+elif [ $# -gt 3 ]; then
+    echo Invalid number of arguments.
+    exit 1
 else
-    echo correct augment 
+    echo Correct number of arguments.
 fi
+
 #verify file exists
-if [ -f "$1"]
+if [-f "$1" ]
     echo file exits
 else   
     echo file invalid
-    break;
+    exit 1;
 fi
 #verify username exists
-if [ grep -r $2 $1 ]:
+if  grep -r "$2" "$1"; then 
     echo found 
 else 
     echo not found
