0b7bf10ccb00c319aa2998075ec36ecba8aeecaa
Gavin Bright <gavinlegomaster5@gmail.com>
2024-03-20 23:29:07 +0000

    I got questions 1-3 fully finished

--- utils.sh
+++ utils.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-for i in $($find /bin -name 'c*'); do
-	echo "$i"
-done
-
+#Question 1
+printf "Here are all of the files in /bin that begin with c.\n"
+echo $(find  -L /bin -name 'c*')
+#Question 2
+printf "Here are all of the files that contain the characters sh.\n"
+echo $(find -L /bin -iname '*sh*')
+#Question 3
+printf "Here are the first 10 file in /usr.\n"
+echo $(find /usr -type f | head)
+#Question 4
+printf "Here are the lines that have model name in the /proc/cpuinfo\n"
 
