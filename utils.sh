04493ca3754c540f3ccc30b3b0fd298d1c218ab3
Gavin Bright <gavinlegomaster5@gmail.com>
2024-03-21 18:06:58 +0000

    I have finished all of the questions and everything works.

--- utils.sh
+++ utils.sh
@@ -12,11 +12,21 @@ echo $(find /usr -type f | head)
 printf "Here are the lines that have model name in the /proc/cpuinfo\n"
 echo $(grep "model name" /proc/cpuinfo)
 #Question 5
-printf "Here are the total number of lines in /etc/services that does not contain the word send\n"
-echo $(grep -v -c send /etc/services)
+printf "Here are the total number of lines in /etc/group that does not contain the word sudo\n"
+echo $(grep -v -c sudo /etc/group)
 #Question 6
-printf "Here are the lines in /etc/services that do contain send and there line numbers also\n"
-echo $(grep -n send /etc/services)
-#For this one, I checked the contents of the file and didn't see any lines where it showed up, but the code should work.
+printf "Here are the lines in /etc/group that do contain sudo and the line numbers also\n"
+echo $(grep -n sudo /etc/group)
 #Question 7
-
+printf "Here is the content of the food file sorted in alphabetical order\n"
+echo $(sort food)
+#Question 8
+printf "Here is the content of the food file sorted in reverse alphabetical order\n"
+echo $(sort food -r)
+#Question 9
+printf "Here is the content of the food file sort in alphabetical order by the second word\n"
+echo $(sort food -k 2)
+#Question 10
+printf "Here is the content of the food file sorted by calories into a file name calo\n"
+sort food -k 4rn > calo
+cat calo
