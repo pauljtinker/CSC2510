6cefb03280feb693bd9537536e88051e41d8885a
Timeking23 <danielgetahun46@gmail.com>
2024-03-26 01:00:50 +0000

    finished utils.sh

--- utils.sh
+++ utils.sh
@@ -2,11 +2,17 @@ find /bin/ -name 'c*'
 find /bin/ -type f -name 'sh*'
 find /usr |head -10
 grep 'model name' /proc/cpuinfo
-grep -cvn 'send' /etc/services cat "dont contain the string send'
+grep -cvn 'send' /etc/services
 grep -cn 'send' /etc/services
 
-cat "------------------in food file-------------------------"
+echo '------------------in food file-------------------------'
 
 cat food |sort
+echo '==================sort in order '
+cat food | sort -r
+echo '==========================sort in reverse order'
 cat food |sort --key=2
-
+echo '====================sort by 2nd name'
+cat food | sort -nr --key=4 > calo
+cat calo
+echo '================sort by calories'
