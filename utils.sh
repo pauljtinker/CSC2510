26cc6a70581cec9e636a761c797c5336cbdcdc9e
Timeking23 <danielgetahun46@gmail.com>
2024-03-26 00:38:37 +0000

    added food and utils

---
+++ utils.sh
@@ -0,0 +1,10 @@
+find /bin/ -name 'c*'
+find /bin/ -type f -name 'sh*'
+find /usr |head -10
+grep 'model name' /proc/cpuinfo
+grep -cvn 'send' /etc/services cat "dont contain the string send'
+grep -cn 'send' /etc/services
+
+cat "------------------in food file-------------------------"
+
+cat food |sort
