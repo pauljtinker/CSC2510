e0327bc039f9648ee83b3ffd79b0bcf114ccd562
JamesE466 <142599832+JamesE466@users.noreply.github.com>
2024-03-26 21:24:40 -0500

    Create utils.sh

---
+++ utils.sh
@@ -0,0 +1,17 @@
+1. find /usr/bin -name '*c'
+
+2. find /usr/bin -type l -name '*sh*'
+
+3. find /usr -type f | head -n 10
+
+4. grep "model name" /proc/cpuinfo
+
+5. grep -v "sudo" /etc/group | wc -l
+
+6. grep "sudo" -n /etc/group
+
+7. sort food
+
+8. sort -r food
+
+9. sort -nrk3,3 -nrk5,5 food > calo
